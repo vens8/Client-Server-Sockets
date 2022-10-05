b95506afe2891f7b6147ba99a7a7d2f07c594a65
vens8 <vensr.maddula@gmail.com>
2022-10-05 19:11:31 +0530

    Update Server.c

--- Sequential/Server.c
+++ Sequential/Server.c
@@ -8,6 +8,9 @@ Rahul Maddula
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
 #define MAX 1024
 #define PORT 8080
 
@@ -22,118 +25,84 @@ long factorial(int x) {
     return fact;
 }
 
-// Function to parse received buffer
-char ** parseBuffer(char * buffer) {
-    char** parsed = NULL;
-    // Split buffer by newline character
-    char * p = strtok(buffer, "\n");
-    int items = 0, i;
-    while (p) {
-        parsed = realloc(parsed, sizeof (char*) * ++items);
-        // If memory allocation fails
-        if (parsed == NULL)
-            exit(-1);
-        parsed[items-1] = p;
-        p = strtok (NULL, "\n");
-    }
-    // Reallocate for the last null element
-    parsed = realloc(parsed, sizeof (char*) * (items + 1));
-    parsed[items] = 0;
-    return parsed;
-}
-
 int main()
 {
-	int serverSocket, clientSocket;
-	struct sockaddr_in serverAddress, clientAddress;
+    // serverSocket = create_socket(serverSocket);
 
-    // File to store results based on client request
+    char buffer[MAX];
+    int serverSocket, clientSocket;
+    struct sockaddr_in serverAddress, clientAddress;
+    socklen_t clientLength;
     FILE *results;
     results = fopen("results.txt", "w");
 
-	// Create server socket
-	serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if (serverSocket == -1) {
-		perror("Couldn't create server socket!\n");
-		exit(0);
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (serverSocket < 0) {
+		printf("Couldn't create server socket!\n");
+		exit(1);
 	}
-	else
-		printf("Successfully created a server socket.\n");
+	printf("Successfully created a server socket.\n");
 	
+	// memset(&serverAddress, '\0', sizeof(serverAddress));
     bzero(&serverAddress, sizeof(serverAddress));
 
+    // Specify address and port of server
 	serverAddress.sin_family = AF_INET;
 	serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
 	serverAddress.sin_port = htons(PORT);
 
 	// Binding the socket to specified IP and port
 	if ((bind(serverSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress))) != 0) {
-		perror("Bind failed!\n");
+		printf("Bind failed!\n");
 		exit(0);
 	}
-	else
-		printf("Socket binding complete.\n");
+	printf("Socket binding complete.\n");
 
 	// Listen for up to 10 clients
 	if ((listen(serverSocket, 10)) != 0) {
-		perror("Listen failed!\n");
+		printf("Listen failed!\n");
 		exit(0);
 	}
 	else
 		printf("Server listening...\n");
-	int clientLength = sizeof(clientAddress);
 
-	// Accept a client
-	clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddress, &clientLength);
-	if (clientSocket < 0) {
-		perror("Accept failed!\n");
-		exit(0);
-	}
-	else
-		printf("Fetched client!\n");
-
-    char buffer[MAX];
-	// Infinite loop that waits for client requests
-	while(1) {
-		bzero(buffer, MAX);
-		// Receive the request buffer from client
-		int readStatus = read(clientSocket, buffer, sizeof(buffer));
-        if (readStatus == -1) {
-            perror("Failed to read from client!\n");
-        }
-        if (strlen(buffer) == 0) {
-            printf("Finished handling requests!\n");
+    while(1) {
+        // Accept a client
+        clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddress, &clientLength);
+        if (clientSocket < 0) {
+            perror("Accept failed!\n");
             exit(0);
         }
-        // Parsing buffer
-        char bufferCopy[MAX];
-        strcpy(bufferCopy, buffer);
-        char** parsed = NULL;
-        parsed = parseBuffer(&bufferCopy);
+        printf("Fetched client on (%s | %d)\n", inet_ntoa(clientAddress.sin_addr), ntohs(clientAddress.sin_port));
 
-        printf("Number received from client: %s\n", parsed[1]);
-        long fact = factorial(atoi(parsed[1]));
-        printf("Factorial calculated: %ld\n", fact);
+        // Infinite loop that communicates with connected client
+        while(1) {
+            // Receive the request buffer from client
+            int readStatus = read(clientSocket, buffer, sizeof(buffer));
+            if (readStatus == -1) {
+                perror("Failed to read from client!\n");
+            }
+            if (strlen(buffer) == 0) {
+                printf("-- Client has disconnected --\n");
+                break;
+            }
+            else {
+                // If client is active and sent something valid, respond with factorial
+                printf("Client sent: %s\n", buffer);
+                long fact = factorial(atoi(buffer));
+                sprintf(buffer, "%ld", fact);
+                printf("Sending to client: %s\n", buffer);
+                write(clientSocket, buffer, sizeof(buffer));
+                bzero(buffer, sizeof(buffer));
+            }
+        }
 
-        // Write to file
-        fprintf(results, "Request No: %s\nRequest Data: %s\nRequest Result: %ld\nPort: %s\nIP Address: %s\n\n", parsed[0], parsed[1], fact, parsed[2], parsed[3]);
-		// printf("Stored the results into /results.txt.\n");
-
-        // Free/zero allocated memory
-        free (parsed);
-        bzero(buffer, MAX);
-        sprintf(buffer, "%ld", fact);
-
-        // Send the computed factorial to the client
-		write(clientSocket, buffer, sizeof(buffer));
-	}
-
-    // Close file
-    fclose(results);
-
-    // Close client socket
-    close(clientSocket);
+        // Close client socket
+        close(clientSocket);
+        printf("Waiting for clients to connect...\n");
+    }
 
 	// Close server socket after handling all requests
 	close(serverSocket);
+    return 0;
 }
